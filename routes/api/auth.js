25cb5d02ee891bdbf2bc90facc2c011cf5e01407
ArchawinWongkittiruk <willypoko@hotmail.com>
2020-08-28 19:06:25 +0700

    Setup express server and routing

---
+++ routes/api/auth.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => res.send('Auth route'));
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+
+// Connect database
+(async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+})();
+
+// Init middleware
+app.use(express.json({ extended: false }));
+
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/boards', require('./routes/api/boards'));
+app.use('/api/lists', require('./routes/api/lists'));
+app.use('/api/cards', require('./routes/api/cards'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log('Server started on port ' + PORT));
