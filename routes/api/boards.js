864726bdbeef3f2248d06fb269e93d8fa8d6f162
ArchawinWongkittiruk <willypoko@hotmail.com>
2020-08-29 02:02:57 +0700

    Do board creation

--- routes/api/boards.js
+++ routes/api/boards.js
@@ -1,6 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
 
-router.get('/', (req, res) => res.send('Boards route'));
+const Board = require('../../models/Board');
+const User = require('../../models/User');
+
+// Create a board
+router.post(
+  '/',
+  [auth, [check('title', 'Title is required').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const { title, backgroundURL } = req.body;
+
+      // Create and save the board
+      const board = await new Board({ title, backgroundURL }).save();
+
+      // Assign the board to the user
+      const user = await User.findById(req.user.id).select('-password');
+      user.ownedBoards.unshift(board.id);
+      await user.save();
+
+      res.json(board);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
