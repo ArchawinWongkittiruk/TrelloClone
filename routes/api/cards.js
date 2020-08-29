ca6d19346e4bf04486275d9decf865f83d4a6925
ArchawinWongkittiruk <willypoko@hotmail.com>
2020-08-30 00:43:10 +0700

    Add card creation

--- routes/api/cards.js
+++ routes/api/cards.js
@@ -1,6 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
 
-router.get('/', (req, res) => res.send('Cards route'));
+const Card = require('../../models/Card');
+const List = require('../../models/List');
+
+// Create a card
+router.post(
+  '/',
+  [auth, [check('title', 'Title is required').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const { title, listId } = req.body;
+
+      // Create and save the card
+      const newCard = new Card({ title });
+      const card = await newCard.save();
+
+      // Assign the card to the list
+      const list = await List.findById(listId);
+      list.cards.unshift(card.id);
+      await list.save();
+
+      res.json(card);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
