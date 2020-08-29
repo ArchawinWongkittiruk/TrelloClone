34eceb4fe19826cb0ca4b10721d4e620df46861c
ArchawinWongkittiruk <willypoko@hotmail.com>
2020-08-29 23:16:31 +0700

    Do list creation

--- routes/api/lists.js
+++ routes/api/lists.js
@@ -1,6 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
 
-router.get('/', (req, res) => res.send('Lists route'));
+const List = require('../../models/List');
+const Board = require('../../models/Board');
+
+// Create a list
+router.post(
+  '/',
+  [auth, [check('name', 'Name is required').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const { name, boardId } = req.body;
+
+      // Create and save the list
+      const newList = new List({ name });
+      const list = await newList.save();
+
+      // Assign the list to the board
+      const board = await Board.findById(boardId);
+      board.lists.unshift(list.id);
+      await board.save();
+
+      res.json(list);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
