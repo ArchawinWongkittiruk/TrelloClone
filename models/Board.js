9e9b877c6c6685266dfcc6ba7bae000983711e3f
ArchawinWongkittiruk <willypoko@hotmail.com>
2020-08-29 15:17:17 +0700

    Add timestamps to Board model

--- models/Board.js
+++ models/Board.js
@@ -1,19 +1,24 @@
 const mongoose = require('mongoose');
 
-const BoardSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  lists: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'lists',
+const BoardSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
     },
-  ],
-  backgroundURL: {
-    type: String,
+    lists: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'lists',
+      },
+    ],
+    backgroundURL: {
+      type: String,
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = Board = mongoose.model('board', BoardSchema);
