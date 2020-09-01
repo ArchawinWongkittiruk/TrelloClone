e9dc1e95db15dcb767449479a54a0fa6f7ef7dc8
ArchawinWongkittiruk <willypoko@hotmail.com>
2020-09-01 22:12:31 +0700

    Redirect to dashboard from home if authenticated

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-const Landing = () => {
+const Landing = ({ isAuthenticated }) => {
+  if (isAuthenticated) {
+    return <Redirect to='/dashboard' />;
+  }
+
   return (
     <section className='landing'>
       <nav className='top'>
@@ -30,4 +37,12 @@ const Landing = () => {
   );
 };
 
-export default Landing;
+Landing.propTypes = {
+  isAuthenticated: PropTypes.bool,
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps)(Landing);
