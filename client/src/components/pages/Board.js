269c52b14fe5960e476b39ba8a6f55692ba40a17
ArchawinWongkittiruk <willypoko@hotmail.com>
2020-09-02 16:12:38 +0700

    Add get board effect to Board

--- client/src/components/pages/Board.js
+++ client/src/components/pages/Board.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { getBoard } from '../../actions/board';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
-const Board = () => {
-  return <div>Board</div>;
+const Board = ({ board, getBoard, match }) => {
+  useEffect(() => {
+    getBoard(match.params.id);
+  }, [getBoard, match.params.id]);
+
+  return !board ? (
+    <CircularProgress />
+  ) : (
+    <section className='board'>
+      <h1>{board.title}</h1>
+    </section>
+  );
 };
 
-export default Board;
+Board.propTypes = {
+  board: PropTypes.object.isRequired,
+  getBoard: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  board: state.board.board,
+});
+
+export default connect(mapStateToProps, { getBoard })(Board);
