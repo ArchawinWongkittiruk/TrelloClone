179a06fd0bf436fd9e97576de3d231207a9940ef
ArchawinWongkittiruk <willypoko@hotmail.com>
2020-09-02 22:04:50 +0700

    Server: Add check to member middleware

--- middleware/member.js
+++ middleware/member.js
@@ -2,8 +2,11 @@ const Board = require('../models/Board');
 
 module.exports = async function (req, res, next) {
   const board = await Board.findById(req.header('boardId'));
-  const members = board.members.map((member) => member.user);
+  if (!board) {
+    return res.status(404).json({ msg: 'Board not found' });
+  }
 
+  const members = board.members.map((member) => member.user);
   if (members.includes(req.user.id)) {
     next();
   } else {
