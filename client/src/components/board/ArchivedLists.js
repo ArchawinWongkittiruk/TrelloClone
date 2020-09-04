4fcab774b72dd476e587fb5a42bbfbdb896262e9
ArchawinWongkittiruk <willypoko@hotmail.com>
2020-09-04 21:26:08 +0700

    Add archived lists board menu section

---
+++ client/src/components/board/ArchivedLists.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import List from '@material-ui/core/List';
+import Button from '@material-ui/core/Button';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+const ArchivedLists = () => {
+  const listObjects = useSelector((state) => state.board.board.listObjects);
+  const dispatch = useDispatch();
+
+  const onSubmit = (listId) => {
+    console.log(listId);
+    // dispatch();
+  };
+
+  return (
+    <div>
+      <List>
+        {listObjects
+          .filter((list) => list.archived)
+          .map((list, index) => (
+            <ListItem key={index}>
+              <ListItemText primary={list.title} />
+              <Button onClick={() => onSubmit(list._id)}>Send to Board</Button>
+            </ListItem>
+          ))}
+      </List>
+    </div>
+  );
+};
+
+export default ArchivedLists;
--- client/src/components/board/BoardDrawer.js
+++ client/src/components/board/BoardDrawer.js
@@ -5,17 +5,20 @@ import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import Button from '@material-ui/core/Button';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import CloseIcon from '@material-ui/icons/Close';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import ArchiveIcon from '@material-ui/icons/Archive';
 
+import ArchivedLists from './ArchivedLists';
 import useStyles from '../../utils/drawerStyles';
 
 const BoardDrawer = () => {
   const classes = useStyles();
   const [open, setOpen] = useState(false);
+  const [viewingArchivedLists, setViewingArchivedLists] = useState(false);
 
   return (
     <div>
@@ -35,21 +38,39 @@ const BoardDrawer = () => {
           paper: classes.drawerPaper,
         }}
       >
-        <div className={classes.drawerHeader}>
-          <h3>Menu</h3>
-          <Button onClick={() => setOpen(false)}>
-            <CloseIcon />
-          </Button>
-        </div>
-        <Divider />
-        <List>
-          <ListItem button>
-            <ListItemIcon>
-              <ArchiveIcon />
-            </ListItemIcon>
-            <ListItemText primary={'Archived Lists'} />
-          </ListItem>
-        </List>
+        {!viewingArchivedLists ? (
+          <div>
+            <div className={classes.drawerHeader}>
+              <h3>Menu</h3>
+              <Button onClick={() => setOpen(false)}>
+                <CloseIcon />
+              </Button>
+            </div>
+            <Divider />
+            <List>
+              <ListItem button onClick={() => setViewingArchivedLists(true)}>
+                <ListItemIcon>
+                  <ArchiveIcon />
+                </ListItemIcon>
+                <ListItemText primary={'Archived Lists'} />
+              </ListItem>
+            </List>
+          </div>
+        ) : (
+          <div>
+            <div className={classes.drawerHeader}>
+              <Button onClick={() => setViewingArchivedLists(false)}>
+                <ChevronLeftIcon />
+              </Button>
+              <h3>Archived Lists</h3>
+              <Button onClick={() => setOpen(false)}>
+                <CloseIcon />
+              </Button>
+            </div>
+            <Divider />
+            <ArchivedLists />
+          </div>
+        )}
         <Divider />
       </Drawer>
     </div>
