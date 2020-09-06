ce12c94fcf89b530f55f231233ab3f01d8b611ee
ArchawinWongkittiruk <willypoko@hotmail.com>
2020-09-07 03:04:03 +0700

    Add delete card action to archived cards and style

--- client/src/components/board/ArchivedCards.js
+++ client/src/components/board/ArchivedCards.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { archiveCard } from '../../actions/board';
+import { archiveCard, deleteCard } from '../../actions/board';
 
+import CardMUI from '@material-ui/core/Card';
 import List from '@material-ui/core/List';
 import Button from '@material-ui/core/Button';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
 
 const ArchivedCards = () => {
   const cards = useSelector((state) => state.board.board.cardObjects);
+  const lists = useSelector((state) => state.board.board.listObjects);
   const dispatch = useDispatch();
 
-  const onSubmit = (cardId) => {
+  const onDelete = async (listId, cardId) => {
+    dispatch(deleteCard(listId, cardId));
+  };
+
+  const onSendBack = async (cardId) => {
     dispatch(archiveCard(cardId, false));
   };
 
@@ -21,9 +26,22 @@ const ArchivedCards = () => {
         {cards
           .filter((card) => card.archived)
           .map((card, index) => (
-            <ListItem key={index}>
-              <ListItemText primary={card.title} />
-              <Button onClick={() => onSubmit(card._id)}>Send to List</Button>
+            <ListItem key={index} className='archived-card'>
+              <CardMUI>{card.title}</CardMUI>
+              <div>
+                <Button
+                  color='secondary'
+                  onClick={() =>
+                    onDelete(
+                      lists.find((list) => list.cards.includes(card._id))._id,
+                      card._id
+                    )
+                  }
+                >
+                  Delete Card
+                </Button>
+                <Button onClick={() => onSendBack(card._id)}>Send to List</Button>
+              </div>
             </ListItem>
           ))}
       </List>
